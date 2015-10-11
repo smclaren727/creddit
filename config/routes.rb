cfacc13fdbdeb364d1261b2802dabce8d0ed5388
smclaren727 <smclaren727@gmail.com>
2015-10-10 20:59:39 -0400

    Completed topics CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
-  resources :posts
-
-  get 'about' => 'welcome/about'
-
+  get 'about' => 'welcome#about'
   root to: 'welcome#index'
 end
